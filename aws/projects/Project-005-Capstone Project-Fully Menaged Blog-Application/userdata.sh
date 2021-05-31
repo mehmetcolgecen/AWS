3f362cf5e630131835132e4dbf32aa769c84e952
C7136-Mehmet <mehmetcolgecen5080@gmail.com>
2021-05-31 21:46:35 +0300

    This is a new commit for what I originally planned to be amended

---
+++ aws/projects/Project-005-Capstone Project-Fully Menaged Blog-Application/userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get update -y
+apt-get install git -y
+apt-get install python3 -y
+cd /home/ubuntu/
+TOKEN="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"
+git clone https://$TOKEN@github.com/mehmetcolgecen/aws_capstone.git
+cd /home/ubuntu/aws_capstone
+apt install python3-pip -y
+apt-get install python3.7-dev libmysqlclient-dev -y
+pip3 install -r requirements.txt
+cd /home/ubuntu/aws_capstone/src
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:80
